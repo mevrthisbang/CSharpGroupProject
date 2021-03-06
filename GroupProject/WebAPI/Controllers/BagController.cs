6641b69146b5e02510c556c34d6d259e5c6d79cb
Hoàng Gia Thiên Phúc <65884017+mevrthisbang@users.noreply.github.com>
2021-03-06 20:39:35 +0700

    Update CRUD method for BagController

--- GroupProject/WebAPI/Controllers/BagController.cs
+++ GroupProject/WebAPI/Controllers/BagController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Description;
 using WebAPI.Models;
 
 namespace WebAPI.Controllers
@@ -18,13 +21,68 @@ namespace WebAPI.Controllers
             {
                 bagID = bag.bagID,
                 bagName = bag.bagName,
-                image=bag.image,
-                price=bag.price,
-                quantity=bag.quantity,
-                size=bag.size,
-                status=bag.status
-            }).Where(bag=>bag.status.Equals("Active")&& bag.quantity > 0
-            ).AsEnumerable();  
+                image = bag.image,
+                price = bag.price,
+                quantity = bag.quantity,
+                size = bag.size,
+                status = bag.status
+            }).Where(bag => bag.status.Equals("Active") && bag.quantity > 0
+            ).AsEnumerable();
+        }
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutEmployee(string id, BagTBL bag)
+        {
+            if (id != bag.bagID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(bag).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BagExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+        private bool BagExists(string id)
+        {
+            return db.BagTBLs.Count(b => b.bagID == id) > 0;
+        }
+        [ResponseType(typeof(BagTBL))]
+        public IHttpActionResult Get(string id)
+        {
+            BagTBL bag = db.BagTBLs.Find(id);
+            if (bag == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(bag);
+        }
+        [HttpPost]
+        public void Add([FromBody] BagTBL bag)
+        {
+            db.BagTBLs.Add(bag);
+        }
+
+        [HttpDelete]
+        public void Delete(string id)
+        {
+            BagTBL bag = db.BagTBLs.SingleOrDefault(x => x.bagID.Equals(id));
+            db.BagTBLs.Remove(bag);
         }
     }
 }
