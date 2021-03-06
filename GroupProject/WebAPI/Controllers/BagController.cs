40b119883d15fa46cec6c474cecac4f7a295b10b
Hoàng Gia Thiên Phúc <65884017+mevrthisbang@users.noreply.github.com>
2021-03-06 09:25:49 +0700

    Add GetActiveBagList method

--- GroupProject/WebAPI/Controllers/BagController.cs
+++ GroupProject/WebAPI/Controllers/BagController.cs
@@ -4,10 +4,27 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WebAPI.Models;
 
 namespace WebAPI.Controllers
 {
     public class BagController : ApiController
     {
+        private BagStoreEntities db = new BagStoreEntities();
+        [HttpGet]
+        public IEnumerable<BagTBL> GetActiveBagList()
+        {
+            return db.BagTBLs.Select(bag => new BagTBL
+            {
+                bagID = bag.bagID,
+                bagName = bag.bagName,
+                image=bag.image,
+                price=bag.price,
+                quantity=bag.quantity,
+                size=bag.size,
+                status=bag.status
+            }).Where(bag=>bag.status.Equals("Active")&& bag.quantity > 0
+            ).AsEnumerable();  
+        }
     }
 }
