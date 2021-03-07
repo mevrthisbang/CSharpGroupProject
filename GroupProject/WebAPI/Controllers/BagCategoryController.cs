7b919f1c599ae5aa840d7010fa7eb40f5946336c
Hoàng Gia Thiên Phúc <65884017+mevrthisbang@users.noreply.github.com>
2021-03-07 08:36:34 +0700

    Update BagCategoryController

--- GroupProject/WebAPI/Controllers/BagCategoryController.cs
+++ GroupProject/WebAPI/Controllers/BagCategoryController.cs
@@ -4,10 +4,17 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WebAPI.Models;
 
 namespace WebAPI.Controllers
 {
     public class BagCategoryController : ApiController
     {
+        private BagStoreEntities db = new BagStoreEntities();
+        [HttpGet]
+        public IEnumerable<CategoryTBL> GetActiveBagList()
+        {
+            return db.CategoryTBLs;
+        }
     }
 }
