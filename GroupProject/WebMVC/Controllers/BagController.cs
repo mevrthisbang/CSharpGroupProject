5d72a3df5e8ff59840f2135625e9a3b4ac126648
Hoàng Gia Thiên Phúc <65884017+mevrthisbang@users.noreply.github.com>
2021-03-14 08:45:34 +0700

    Update

--- GroupProject/WebMVC/Controllers/BagController.cs
+++ GroupProject/WebMVC/Controllers/BagController.cs
@@ -47,7 +47,7 @@ namespace WebMVC.Controllers
                     lastID = "B_" + count;
                 }
                 string extension = Path.GetExtension(ImageFile.FileName);
-                string filename = lastID + extension;
+                string filename = lastID + "_" + DateTime.Now.ToString("yyyyMMdd") + extension;
                 bag.Image = "http://localhost:55575/img/" + filename;
                 WCFFileServiceClient fileServiceClient = new WCFFileServiceClient();
                 fileServiceClient.UploadFile(filename,ImageFile.InputStream);
@@ -73,7 +73,7 @@ namespace WebMVC.Controllers
             {
                 
                 string extension = Path.GetExtension(ImageFile.FileName);
-                string filename = bag.BagID +DateTime.Now.ToString("yyyyMMdd")+ extension;
+                string filename = bag.BagID +"_"+DateTime.Now.ToString("yyyyMMdd")+ extension;
                 bag.Image = "http://localhost:55575/img/" + filename;
                 WCFFileServiceClient fileServiceClient = new WCFFileServiceClient();
                 fileServiceClient.UploadFile(filename, ImageFile.InputStream);
